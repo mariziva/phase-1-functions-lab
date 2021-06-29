092d09b3bb14ce7b71b4d12f8af7d7fac0338a07
Mari Taksa <welch.mari@gmail.com>
2021-06-29 09:55:59 -0500

    commit tree 0dbe8f4220927009c787e252b68e33b2089d7244

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pickupLocation) {
+    return (Math.abs(pickupLocation - 42))
+}
+
+function distanceFromHqInFeet (pickupLocation) {
+    return (distanceFromHqInBlocks(pickupLocation) * 264)
+}
+
+function distanceTravelledInFeet (pickupLocation, dropoffLocation) {
+    return (Math.abs(pickupLocation-dropoffLocation)*264)
+}
+
+function calculatesFarePrice (pickupLocation, dropoffLocation) {
+    if (distanceTravelledInFeet(pickupLocation, dropoffLocation)<= 400)
+    return 0;
+
+    else if (distanceTravelledInFeet(pickupLocation, dropoffLocation) > 2500)
+    return "cannot travel that far";
+    
+    else if (2000 >= (distanceTravelledInFeet(pickupLocation, dropoffLocation)))
+    return (((distanceTravelledInFeet(pickupLocation, dropoffLocation))-400)*0.02);
+    
+    else if (2000 < distanceTravelledInFeet(pickupLocation, dropoffLocation) <= 2500)
+    return 25;
+    
+    
+}
